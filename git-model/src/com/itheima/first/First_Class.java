ae180e23378f12636ec4f09cb8dd55b175d4c71f
lxy <1508399732@qq.com>
2020-05-28 15:45:54 +0800

    开发员2做了一点修改

--- git-model/src/com/itheima/first/First_Class.java
+++ git-model/src/com/itheima/first/First_Class.java
@@ -1,9 +1,10 @@
 package com.itheima.first;
 
 public class First_Class {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("加油");
     }
+    //开发员2做了修改
     public void test(){
         System.out.println(" hello");
     }
