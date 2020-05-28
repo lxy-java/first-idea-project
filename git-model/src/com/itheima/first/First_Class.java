7fe54710931b93499f96955a395d6bdd7d0d5cd3
lxy <1508399732@qq.com>
2020-05-28 15:23:06 +0800

    修改了一点内容

--- git-model/src/com/itheima/first/First_Class.java
+++ git-model/src/com/itheima/first/First_Class.java
@@ -4,4 +4,7 @@ public class First_Class {
     public static void main(String[] args){
         System.out.println("加油");
     }
+    public void test(){
+        System.out.println(" hello");
+    }
 }
